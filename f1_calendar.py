9aeafd6c50144727bb238ae7dedd8934bf35c454
danielrodgersuk <djr@danielrodgers.uk>
2021-04-05 22:30:12 +0100

    Update f1_calendar.py

--- f1_calendar.py
+++ f1_calendar.py
@@ -4,18 +4,27 @@ import pandas as pd
 
 def Calendar(year):
 
-	SeasonHTML = requests.get(f"https://en.wikipedia.org/wiki/{year}_Formula_One_season")
+	Season_HTML = requests.get(f"https://en.wikipedia.org/wiki/{year}_Formula_One_season")
 
-	SeasonSoup = BeautifulSoup(SeasonHTML.text, 'html.parser')
+	Season_Soup = BeautifulSoup(Season_HTML.text, 'html.parser')
 
-	SeasonCalendar_Soup = SeasonSoup.find('table',{'class':'wikitable'}).findAll('tr')
+	Season_Calendar_Soup = Season_Soup.find('table',{'class':'wikitable'}).findAll('tr')
 
-	SeasonCalendar_Lists = [] 
-	for race in SeasonCalendar_Soup:
-		SeasonCalendar_Lists.append(race.text.strip().split('\n\n'))
+	Season_Calendar_Lists = [] 
+	for race in Season_Calendar_Soup:
+		Season_Calendar_Lists.append(race.text.strip().split('\n\n'))
 
-	return pd.DataFrame(SeasonCalendar_Lists)
+	Season_Calendar_Header = Season_Calendar_Lists[0]
+	Season_Calendar_Lists = Season_Calendar_Lists[1:]
+
+	Calendar = pd.DataFrame(Season_Calendar_Lists, columns=Season_Calendar_Header)
+	Calendar['Date'] = pd.to_datetime(Calendar['Date'] + ' ' + f'{year}')
+	return Calendar[['Rnd','Date','Race','Circuit']]
 
 def Calendars(year1, year2):
+	calendars = pd.DataFrame(columns=['Rnd','Date','Race','Circuit'])
 	for year in range(1+year2-year1):
-		print(Calendar(year+year1))
+		calendars = calendars.append(Calendar(year+year1),ignore_index=True)
+	return calendars
+
+print(Calendars(1950,1963))
