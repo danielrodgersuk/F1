b037d23171dc473d6282cce0ace2960865c356da
danielrodgersuk <djr@danielrodgers.uk>
2021-04-05 20:13:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 Season_1950_HTML = requests.get("https://en.wikipedia.org/wiki/1950_Formula_One_season")
 
 Season_1950_Soup = BeautifulSoup(Season_1950_HTML.text, 'html.parser')
 
-Season_1950_Calendar_Soup_Header = Season_1950_Soup.find('table',{'class':'wikitable'}).findAll('th')
-Season_1950_Calendar_Soup_Text = Season_1950_Soup.find('table',{'class':'wikitable'}).findAll('a')
+Season_1950_Calendar_Soup = Season_1950_Soup.find('table',{'class':'wikitable'}).findAll('tr')
 
-for header in Season_1950_Calendar_Soup_Header:
-	print(header)
+Season_1950_Calendar_Lists = [] 
+for race in Season_1950_Calendar_Soup:
+	Season_1950_Calendar_Lists.append(race.text.strip().split('\n\n'))
 
-for race in Season_1950_Calendar_Soup_Text:
-	print(race.get("title"))
+Season_1950_Calendar = pd.DataFrame(Season_1950_Calendar_Lists)
