10d976951646694d1a23666eb5a914f6cadfd463
danielrodgersuk <djr@danielrodgers.uk>
2021-04-05 19:36:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ Season_1950_HTML = requests.get("https://en.wikipedia.org/wiki/1950_Formula_One_
 
 Season_1950_Soup = BeautifulSoup(Season_1950_HTML.text, 'html.parser')
 
-Season_1950_Calendar_Soup = Season_1950_Soup.find('table',{'class':'wikitable'}).findAll('a')
+Season_1950_Calendar_Soup_Header = Season_1950_Soup.find('table',{'class':'wikitable'}).findAll('th')
+Season_1950_Calendar_Soup_Text = Season_1950_Soup.find('table',{'class':'wikitable'}).findAll('a')
 
-for race in Season_1950_Calendar_Soup:
+for header in Season_1950_Calendar_Soup_Header:
+	print(header)
+
+for race in Season_1950_Calendar_Soup_Text:
 	print(race.get("title"))
