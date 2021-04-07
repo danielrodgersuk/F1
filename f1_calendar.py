eb40aaedb529fe1060a1da3c1ff9a152ac29461f
danielrodgersuk <djr@danielrodgers.uk>
2021-04-07 01:06:07 +0100

    Update f1_calendar.py

--- f1_calendar.py
+++ f1_calendar.py
@@ -4,27 +4,35 @@ import pandas as pd
 
 def Calendar(year):
 
-	Season_HTML = requests.get(f"https://en.wikipedia.org/wiki/F1_{year}")
+	Season_HTML = requests.get(f"https://www.formula1.com/en/results.html/{year}/races.html")
 
-	Season_Soup = BeautifulSoup(Season_HTML.text, 'html.parser')
+	Season_Soup = BeautifulSoup(Season_HTML.text, "html.parser")
 
-	Season_Calendar_Soup = Season_Soup.find('table',{'class':'wikitable'}).findAll('tr')
+	Season_Calendar_Soup = Season_Soup.find("table",{"class":"resultsarchive-table"}).findAll("tr")
 
-	Season_Calendar_Lists = [] 
+	Season_Calendar_Header = []
+	Season_Calendar_Races = []
+	
 	for race in Season_Calendar_Soup:
-		Season_Calendar_Lists.append(race.text.strip().split('\n\n'))
+		Season_Calendar_Fields = race.findAll("th")
+		for field in Season_Calendar_Fields:
+			Season_Calendar_Header.append(field.text.strip())
 
-	Season_Calendar_Header = Season_Calendar_Lists[0]
-	Season_Calendar_Lists = Season_Calendar_Lists[1:]
+		Season_Calendar_Lists = race.findAll("td")
+		Season_Calendar_Race = []
+		for field in Season_Calendar_Lists:
+			Season_Calendar_Race.append(field.text.strip().replace("\n", " "))
+		Season_Calendar_Races.append(Season_Calendar_Race)
+	
+	Calendar = pd.DataFrame(Season_Calendar_Races, columns=Season_Calendar_Header)
 
-	Calendar = pd.DataFrame(Season_Calendar_Lists, columns=Season_Calendar_Header)
-	Calendar['Date'] = pd.to_datetime(Calendar['Date'] + ' ' + f'{year}')
-	return Calendar[['Rnd','Date','Race','Circuit']]
-
+	return Calendar
+    
 def Calendars(year1, year2):
-	calendars = pd.DataFrame(columns=['Rnd','Date','Race','Circuit'])
+	calendars = pd.DataFrame()
 	for year in range(1+year2-year1):
 		calendars = calendars.append(Calendar(year+year1),ignore_index=True)
 	return calendars
 
-print(Calendars(1950,1963))
+print(Calendars(1950,2021))
+# Next step: deal with none
